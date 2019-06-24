2e15504b507b621a65f5d63c9995188750014c88
Austin Clyde <aclyde@uchicago.edu>
2019-06-24 16:25:33 -0400

    Updated to show off fancy stuff

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import numpy as np
 from torch import optim
 from torch.utils import data
 from torch import nn
+import torch
+from tqdm import tqdm
 # return a single sample perfectly
 class DataSet(data.Dataset):
     def __init__(self, x, y):
@@ -29,31 +31,63 @@ y_test = f(X_test)
 
 ## NP Arrays
 
-train_loader = data.DataLoader(dataset=DataSet(X, y), batch_size=32, shuffle=True, drop_last=True)
-test_loader = data.DataLoader(dataset=DataSet(X_test, y), batch_size=32, shuffle=True, drop_last=True)
+train_loader = data.DataLoader(dataset=DataSet(X, y), batch_size=32, shuffle=True, drop_last=True, num_workers=2)
+test_loader = data.DataLoader(dataset=DataSet(X_test, y), batch_size=32, shuffle=True, drop_last=True, num_workers=2)
+
+
 
 
 model = VAE()
-optimizer = optim.SGD(model.parameters(), lr=0.0001)
+optimizer = optim.SGD(model.parameters(), lr=0.0001) #used to optimize model
 
-loss_function = nn.MSELoss(reduce='mean')
+loss_function = nn.MSELoss(reduce='mean') #reduce means how you combine the loss across the batch
 
 
 
 for epoch in range(10):
-    for i, (x_batch, y_batch) in enumerate(train_loader):
-        x_batch = x_batch.float()
+    ##fancy way to show off training:
+
+    tqdm_data = tqdm(train_loader,
+                     desc='Training (epoch #{})'.format(epoch))
+    model.train()
+    #training loop
+    for i, (x_batch, y_batch) in enumerate(tqdm_data):
+        x_batch = x_batch.float() #convert data to FP32
         y_batch = y_batch.float()
 
-        x_batch = x_batch.view(32, 1)
-        y_batch = y_batch.view(32, 1)
+        x_batch = x_batch.view(32, 1) # only needed because the input is of size 1, normally not needed
+        y_batch = y_batch.view(32, 1) # only needed because the input is of size 1, normally not needed
 
 
-        optimizer.zero_grad()
-        y_pred = model(x_batch)
+        optimizer.zero_grad() # Need to clear out gradients before computing on batch!
+        y_pred = model(x_batch) #run data through model
 
-        loss = loss_function(y_pred, y_batch)
-        print(loss.item())
-        loss.backward()
-        optimizer.step()
+        loss = loss_function(y_pred, y_batch) #compute loss
+        loss.backward() #compute gradients
+        optimizer.step() #take a step of gradients * lr
 
+        loss_value = loss.item()
+        postfix = [f'loss={loss_value:.5f}']
+        tqdm_data.set_postfix_str(' '.join(postfix))
+
+    tqdm_data = tqdm(train_loader,
+                     desc='Validation (epoch #{})'.format(epoch))
+    #validation loop
+    model.eval()
+    with torch.no_grad():
+        for i, (x_batch, y_batch) in enumerate(tqdm_data):
+            x_batch = x_batch.float()
+            y_batch = y_batch.float()
+
+            x_batch = x_batch.view(32, 1) # only needed because the input is of size 1, normally not needed
+            y_batch = y_batch.view(32, 1) # only needed because the input is of size 1, normally not needed
+
+
+            optimizer.zero_grad() # Need to clear out gradients before computing on batch!
+            y_pred = model(x_batch) #run data through model
+
+            loss = loss_function(y_pred, y_batch) #compute loss
+
+            loss_value = loss.item()
+            postfix = [f'loss={loss_value:.5f}']
+            tqdm_data.set_postfix_str(' '.join(postfix))
